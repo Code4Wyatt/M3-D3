084ea54cf150d0dce68ddb48a976c0d22b192daf
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-09-15 20:05:27 +0100

    Updated

--- m3d3-main/js.js
+++ m3d3-main/js.js
@@ -45,7 +45,7 @@ for(let card of dataBase.photos){ // using a for loop to iterate through each it
     rowCard.innerHTML += `
     <div class="col-md-4 mb-4">
     <div class="card mb-4 shadow-sm h-100">
-    <img src="${card.src.large}" alt="" style="height: 10rem;  object-fit: cover;">
+    <img src="${card.src.large}" alt="" style="height: 15rem;  object-fit: cover;">
       <div class="card-body">
         <p class="card-text">${card.photographer}</p>
         <div
@@ -63,6 +63,10 @@ for(let card of dataBase.photos){ // using a for loop to iterate through each it
               class="btn btn-sm btn-outline-secondary" onclick="hideCard(this)">
               Hide
             </button>
+            <a onclick="downloadImg(this.src.large)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></a>
           </div>
           <small class="text-muted">id:${card.id}</small>
         </div>
@@ -74,18 +78,21 @@ for(let card of dataBase.photos){ // using a for loop to iterate through each it
 alertM()
 }
 
-// second Button
+// Second button
+
 function loadSecond(){
     loadData()
     setTimeout(loadImgs, 500)
     
 }
+
 // load from database
+
 function loadData() {
     fetch(`${dataBase.next_page}`, {
         "method": "GET",
         "headers": {
-            "Authorization": "bearer 563492ad6f917000010000010c07485ab4c945a295e08a494dec09ff"
+            "Authorization": "bearer 563492ad6f91700001000001fe7f1bfa604c4313af35b3f3b8ea9b23"
         }
     })
         .then(response => response.json())
@@ -93,20 +100,21 @@ function loadData() {
         .catch(err => alert(err))
 }
 
-// Hide Card
+// Hide card
 
-function hideCard(inf) { // passing the element as a parameter 
-  const card = inf.parentElement.parentElement.parentElement.parentElement.parentElement // targetting the card by assigning the element (the button itself) to 5 parentElements as it is contained within 5 divs
+function hideCard(clickedBtn) { // passing the element as a parameter 
+  const card = clickedBtn.parentElement.parentElement.parentElement.parentElement.parentElement // targetting the card by assigning the element (the button itself) to 5 parentElements as it is contained within 5 divs
   
   card.remove() // removing the card
 }
 
-// SEARCH
+// Search
+
 function searchIt(value) {
     fetch(`https://api.pexels.com/v1/search?query=`+value.toLowerCase(), {
         "method": "GET",
         "headers": {
-            "Authorization": "bearer 563492ad6f917000010000010c07485ab4c945a295e08a494dec09ff"
+            "Authorization": "bearer 563492ad6f91700001000001fe7f1bfa604c4313af35b3f3b8ea9b23"
         }
     })
         .then(response => response.json())
@@ -114,7 +122,7 @@ function searchIt(value) {
         .catch(err => alert(err))
 
         rowCard.innerHTML =  ''
-        setTimeout(loadImgs, 500)
+        setTimeout(loadImgs, 100)
 }
 
 
@@ -123,7 +131,7 @@ function searchIt(value) {
 function alertM(m){
     let body = document.querySelector('.jumbotron .container')
     body.innerHTML += `
-    <div class="alert alert-secondary  w-50 text-center" role="alert" style="margin: auto !important">
+    <div class="alert alert-secondary  w-50 text-center" role="alert" style="margin: auto !important; transition: 1s">
         Uploaded ${dataBase.photos.length} images
     </div>`
     setTimeout(()=>{
